9a53f16a90e7fbaa96b40651e80e6bb7dfa44163
MarissaShieh <marissa.shieh@gmail.com>
2019-02-19 11:50:26 -0800

    Implemented changes to getElementsByClassName.js without helper function

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,19 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className, node) {
+  node = node || document.body;
+  var results = [];
+  
+  var names = node.className.split(' ');
+  if (names.indexOf(className)>=0){
+    results.push(node);
+  }
+
+  for (let i = 0; i<node.children.length;i++){
+    var final = getElementsByClassName(className, node.children[i]);
+    results = results.concat(final);
+  }
+  console.log("Our results = "+ results);
+  return results;
 };
